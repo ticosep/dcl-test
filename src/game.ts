dc07ab919293a184987f0eafc856c48354e5fdb6
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2022-08-16 21:03:55 -0300

    add event listener to model interact

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,25 @@
-import { MoveEntity } from './utils/MoveEntity';
+import { MoveEntity } from './utils/MoveEntity'
 
-const entity = new Entity();
-entity.addComponent(new GLTFShape('models/speaker_player.glb'));
-entity.addComponent(new Transform({ position: new Vector3(5, 1, 5) }));
-engine.addEntity(entity);
+const entity = new Entity()
+entity.addComponent(new GLTFShape('models/speaker_player.glb'))
+entity.addComponent(
+  new Transform({
+    position: new Vector3(0, 1, 0),
+    rotation: new Quaternion(0, 0, 0, 0),
+    scale: new Vector3(3, 3, 3),
+  }),
+)
 
-const moveEntity = new MoveEntity(entity);
-engine.addSystem(moveEntity);
+entity.addComponent(
+  new OnPointerDown((e) => {
+    const streamSource = new Entity()
+    streamSource.addComponent(
+      new AudioStream('https://edge.singsingmusic.net/MC2.mp3'),
+    )
+    engine.addEntity(streamSource)
+
+    streamSource.getComponent(AudioStream).playing = true
+  }),
+)
+
+engine.addEntity(entity)
--- src/utils/MoveEntity.ts
+++ src/utils/MoveEntity.ts
@@ -1,15 +1,7 @@
 export class MoveEntity implements ISystem {
-  entity: Entity;
-
+  entity: Entity
   constructor(entity: Entity) {
-    this.entity = entity;
+    this.entity = entity
   }
-
-  update() {
-    const transform = this.entity.getComponent(Transform);
-    const playerPosition = Camera.instance.position;
-
-    transform.rotation = new Quaternion(0, 90, 0, 0);
-    transform.position = playerPosition.add(new Vector3(0.6, 0.2, 0.2));
-  }
+  update() {}
 }
